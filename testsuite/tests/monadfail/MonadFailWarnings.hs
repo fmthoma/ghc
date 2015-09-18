523e93d12527d549eb378f199abec3a39930fd91
David Luposchainsky <dluposchainsky@gmail.com>
2015-09-18 16:10:56 +0200

    Expand testsuite

--- testsuite/tests/monadfail/MonadFailWarnings.hs
+++ testsuite/tests/monadfail/MonadFailWarnings.hs
@@ -3,6 +3,8 @@ module MonadFailWarnings where
 import Data.Functor.Identity
 import Control.Monad.ST
 
+
+
 -- should warn, because the do-block gets a general Monad constraint,
 -- but should have MonadFail
 general :: Monad m => m a
@@ -10,11 +12,23 @@ general = do
     Just x <- undefined
     undefined
 
+
+
+-- should NOT warn, because the constraint is correct
+general' :: MonadFail m => m a
+general' = do
+    Just x <- undefined
+    undefined
+
+
+
 -- should warn, because Identity isn't MonadFail
 identity :: Identity a
 identity = do
     Just x <- undefined
     undefined
+
+
 
 -- should NOT warn, because IO is MonadFail
 io :: IO a
@@ -22,11 +36,23 @@ io = do
     Just x <- undefined
     undefined
 
+
+
 -- should warn, because (ST s) is not MonadFail
 st :: ST s a
 st = do
     Just x <- undefined
     undefined
+
+
+
+-- should warn, because (r ->) is not MonadFail
+reader :: r -> a
+reader = do
+    Just x <- undefined
+    undefined
+
+
 
 -- should NOT warn, because matching against newtype
 newtype Newtype a = Newtype a
@@ -35,12 +61,16 @@ newtypeMatch = do
     Newtype x <- undefined
     undefined
 
+
+
 -- should NOT warn, because Data has only one constructor
 data Data a = Data a
 singleConMatch :: Identity a
 singleConMatch = do
     Data x <- undefined
     undefined
+
+
 
 -- should NOT warn, because patterns always match
 test6, test7, test8, test9 :: Monad m => m a
